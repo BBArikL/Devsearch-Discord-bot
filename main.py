e65d0256a3b059af72f2a047aff2cda549151b8e
Arik <chakhoury123@outlook.com>
2021-01-19 19:59:13 +0000

    Adding basic error detection + started doing json file implementation (v0.4.1)

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import discord #Discord libraries
 import os
+import json #Support for json files
 from keepalive import keep_alive # imports the web server that pings the bot continually
 from discord.ext import commands
 
 client = discord.Client() # Connects to the discord client
 client = commands.Bot(command_prefix = '&')
 discord.ext.commands.Bot(command_prefix = '&', case_insensitive = True)
+#client.removecommand("help")
 
-# Lists of Docs and docs link
-docs_link = {
+docs_link = { 
 "Unity" : "https://docs.unity3d.com/Manual/index.html",
 "Python" : "https://docs.python.org/3/", 
 "Xojo" : "http://docs.xojo.com/", 
@@ -17,12 +18,18 @@ docs_link = {
 "Unrealengine" :  "https://docs.unrealengine.com/en-US/index.html",
 "C++" : "https://docs.microsoft.com/en-us/cpp/",
 "C#" : "https://docs.microsoft.com/en-us/dotnet/csharp/",
-"Discord" : "https://discord.com/developers/docs/intro and https://discordpy.readthedocs.io/en/latest/index.html",
+"Discord" : "https://discord.com/developers/docs/intro",
 "Pascal" : "https://www.freepascal.org/docs-html/3.0.0/prog/prog.html",
-"Quiskit" : "https://qiskit.org/documentation/",
 "idk" : "Then why did you asked? :thinking:",
 "Cake": "The cake is not a lie.", # little secret hehehe
-}
+ } 
+
+
+def load_json(): # Returns the dictionary in the specified json file
+  with open('docs_links.json', 'r') as f:
+    dictionary = json.load(f)
+
+  return dictionary
 
 @client.event #Callback to a unsychronous library of events
 async def on_ready():
@@ -31,12 +38,32 @@ async def on_ready():
 
   print('Logged in as {0.user}'.format(client))
 
+@client.event
+async def on_command_error(ctx, error):
+  #Handles errors
+  if isinstance(error, commands.CommandNotFound): # Command not found
+    await ctx.send('Invalid command. Try &help to search for usable commands.')
+  elif isinstance(error, commands.MissingRequiredArgument):
+    await ctx.send('A required argument is needed. Try &help to see required arguments.')
+  
+  await ctx.send('Error')
+
 @client.command()
 async def rtfm(ctx): # Brief intro to the bot
   await ctx.send('I am there to save you, ask me someting with &Dev <language type>!\nSearching for a topic on StackOverflow? Type &Stack <Question>!\nWant more commands? Type &Dev help!\nWant to help the bot? Type &Git!\nAnd thanks to all the ones that made this idea possible! Type &Credits!')
 
+"""@client.group(invoke_without_command=True)
+async def help(ctx):
+  em = discord.Embed(title = "Devsearch", dscription = f'Use {client.command_prefix}Dev <Programing language> to use the bot')
+  em.addfield(name="Dev", value="'C++', 'C#', 'Java'")
+  
+  await ctx.send(embed = em)
+"""
+
 @client.command()
 async def dev(ctx, *, question=None): # Checks the documentation of a certain app/language
+  #docs_link = load_json
+
   if any(word in question.split(" ")[0] for word in docs_link): # Finds the documentation to check in
     word = question.split(" ")[0]
 
