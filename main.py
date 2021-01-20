cd37c8a498fd98a4eed69232caa620d21be767ff
Arik <chakhoury123@outlook.com>
2021-01-20 00:47:53 +0000

    Finished adding json file + Started Help commands (v0.4.2)

--- main.py
+++ main.py
@@ -7,29 +7,7 @@ from discord.ext import commands
 client = discord.Client() # Connects to the discord client
 client = commands.Bot(command_prefix = '&')
 discord.ext.commands.Bot(command_prefix = '&', case_insensitive = True)
-#client.removecommand("help")
-
-docs_link = { 
-"Unity" : "https://docs.unity3d.com/Manual/index.html",
-"Python" : "https://docs.python.org/3/", 
-"Xojo" : "http://docs.xojo.com/", 
-"Java" : "https://docs.oracle.com/javase/8/docs/api/index.html?overview-summary.html",
-"UE4" :  "https://docs.unrealengine.com/en-US/index.html",
-"Unrealengine" :  "https://docs.unrealengine.com/en-US/index.html",
-"C++" : "https://docs.microsoft.com/en-us/cpp/",
-"C#" : "https://docs.microsoft.com/en-us/dotnet/csharp/",
-"Discord" : "https://discord.com/developers/docs/intro",
-"Pascal" : "https://www.freepascal.org/docs-html/3.0.0/prog/prog.html",
-"idk" : "Then why did you asked? :thinking:",
-"Cake": "The cake is not a lie.", # little secret hehehe
- } 
-
-
-def load_json(): # Returns the dictionary in the specified json file
-  with open('docs_links.json', 'r') as f:
-    dictionary = json.load(f)
-
-  return dictionary
+client.remove_command("help")
 
 @client.event #Callback to a unsychronous library of events
 async def on_ready():
@@ -43,42 +21,54 @@ async def on_command_error(ctx, error):
   #Handles errors
   if isinstance(error, commands.CommandNotFound): # Command not found
     await ctx.send('Invalid command. Try &help to search for usable commands.')
-  elif isinstance(error, commands.MissingRequiredArgument):
+  elif isinstance(error, commands.MissingRequiredArgument): # Manque d'arguments
     await ctx.send('A required argument is needed. Try &help to see required arguments.')
-  
-  await ctx.send('Error')
+  else: # Erreur non supporté pour le moment
+    await ctx.send('Error not defined. Please report this issue at https://github.com/Noobyprogrammer/Devsearch-Discord-bot')
 
 @client.command()
 async def rtfm(ctx): # Brief intro to the bot
   await ctx.send('I am there to save you, ask me someting with &Dev <language type>!\nSearching for a topic on StackOverflow? Type &Stack <Question>!\nWant more commands? Type &Dev help!\nWant to help the bot? Type &Git!\nAnd thanks to all the ones that made this idea possible! Type &Credits!')
 
-"""@client.group(invoke_without_command=True)
+@client.group(invoke_without_command=True, case_insensitive = True)
 async def help(ctx):
-  em = discord.Embed(title = "Devsearch", dscription = f'Use {client.command_prefix}Dev <Programing language> to use the bot')
-  em.addfield(name="Dev", value="'C++', 'C#', 'Java'")
-  
-  await ctx.send(embed = em)
-"""
+  embed=discord.Embed(title="Devsearch", url="https://github.com/Noobyprogrammer/Devsearch-Discord-bot", description="Search into Application/Languages documentation and provides fast help to devs searching for solutions in their ideas.")
+  embed.add_field(name="Dev", value="Use &Dev <language> to get information on links to the documentation. You can also add a keyword to precise your search! For supported documentations, use &help Dev.", inline=False)
+  embed.add_field(name="Stack", value="Search for a question in stackoverflow with &Stack <Question>!", inline=True)
+  embed.add_field(name="Git", value="Come help the bot! &Git", inline=True)
+  embed.set_footer(text="Support the bot here: https://github.com/Noobyprogrammer/Devsearch-Discord-bot")
+  await ctx.send(embed=embed)
+
+@help.command()
+async def devcommand(ctx):
+  embed=discord.Embed(title="Dev", description="Search the documentation of various languages and applications.")
+  embed.add_field(name="C++", value="C++ documentation", inline=True)
+  embed.add_field(name="C#", value="C# documentation", inline=True)
+  embed.add_field(name="Java", value="Java documentation", inline=True)
+  embed.add_field(name="Python", value="Python documentation", inline=True)
+  embed.add_field(name="Pascal", value="Pascal documentation", inline=True)
+  embed.add_field(name="Xojo", value="Xojo documentation", inline=True)
+  embed.add_field(name="Unity", value="Unity documentation", inline=True)
+  embed.add_field(name="UE4", value="UE4 documentation", inline=True)
+  embed.add_field(name="Discord", value="Discord documentation", inline=True)
+  embed.add_field(name="Qiskit", value="Qiskit documentation", inline=True)
+  embed.add_field(name="**Syntax**", value="&Dev <Programming language> [Keywords]", inline=False)
+  embed.set_footer(text="Support the bot here: https://github.com/Noobyprogrammer/Devsearch-Discord-bot")
+  await ctx.send(embed=embed)
 
 @client.command()
 async def dev(ctx, *, question=None): # Checks the documentation of a certain app/language
-  #docs_link = load_json
+  # Lis le fichier json
+  with open('docs_links.json', 'r') as f:
+    docs_link = json.load(f)
 
-  if any(word in question.split(" ")[0] for word in docs_link): # Finds the documentation to check in
-    word = question.split(" ")[0]
+  docname = question.split(" ")[0]
 
-    #tries to catch syntax errors in Discord????
-    #if (word for word in docs_link):
-    await ctx.send('Here is some documentation: ' + docs_link[word])
-      
-  elif (question.split(" ")[0] == "help"):
-    await ctx.send("welp... you asked for help but i can only give you the satisfaction that devs are as clueless as you") 
-    #we need to write documentation here but Idk what
-    #All the commands formatted in a box
-      
-  else:
-    # Be polite :)
-    await ctx.send("Request not understood.... try '&dev help' for commands")
+  try:
+    docs_link[docname]
+    await ctx.send('Here is some documentation: ' + docs_link[docname])
+  except ValueError:
+    await ctx.send(f'Request not understood.... try {client.command_prefix}help dev for commands')
 
 @client.command()
 async def stack(ctx, *, question=None): #StackOverflow questions
